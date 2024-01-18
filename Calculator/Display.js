f45b490fb43aa098f9da0129408a9d40050fed5c
Tania Ludueña <Tanialuduenaok@gmail.com>
2024-01-18 18:53:12 -0300

    Grabo cambios

--- Calculator/Display.js
+++ Calculator/Display.js
@@ -3,52 +3,50 @@ class Display {
         this.displayPreviousValue = displayPreviousValue;
         this.displayActualValue = displayActualValue;
         this.calculator = new Calculator();
-        this.operationType = undefined; // Guarda el tipo de operación del usuario
+        this.operationType = undefined;
         this.previousValue = '';
         this.actualValue = '';
-        this.signs = { sum: '+', division: '%', multiplication: 'x', substraction: '-' };
+        this.signs = { sum: '+', division: '%', multiplication: 'x', subtraction: '-' };
     }
 
-    deleted() { //se encarga de eliminar el ultimo caracter de actualValue
+    deleted() {
         this.actualValue = this.actualValue.toString().slice(0, -1);
         this.printValues();
     }
 
-    deletedAll() { // restablece actualValue, previousValue, y operationType a sus valores iniciales (vacíos o indefinidos).
+    deletedAll() {
         this.actualValue = '';
         this.previousValue = '';
-        this.operationType = undefined; 
+        this.operationType = undefined;
         this.printValues();
     }
 
-    //lo utilize para agregar un número (o un punto decimal) a actualValue, siempre y cuando no haya otro punto decimal presente.
     addNumber(number) {
         if (number === '.' && this.actualValue.includes('.')) return;
-        this.actualValue.push(number.toString());
+        this.actualValue = this.actualValue.toString() + number.toString();
         this.printValues();
     }
-    //Después de actualizar actualValue, se llama a printValues para reflejar los cambios en la interfaz de usuario.
-    // actualiza los elementos DOM (displayPreviousValue y displayActualValue) con los valores actuales de previousValue, operationType, y actualValue.
+
     printValues() {
-        this.displayActualValue.textContent = this.actualValue.join('');
+        this.displayActualValue.textContent = this.actualValue;
         this.displayPreviousValue.textContent = `${this.previousValue} ${this.signs[this.operationType] || ''}`;
     }
 
     calculate() {
-        const previousValue = parseFloat(this.previousValue); //convierte previousValue a numeros parseFloat
+        const previousValue = parseFloat(this.previousValue);
         const actualValue = parseFloat(this.actualValue);
 
         if (isNaN(actualValue) || isNaN(previousValue)) return;
-        
-        this.actualValue = this.calculator[this.operationType](previousValue, actualValue); //
+
+        this.actualValue = this.calculator[this.operationType](previousValue, actualValue);
         this.printValues();
     }
 
-    compute(type) { //realiza la operacion actual
+    compute(type) {
         this.calculate();
         this.operationType = type;
-        this.previousValue = this.actualValue || this.previousValue; 
-        this.actualValue = ''; //inicializa una cadena vacia para comenzar la siguiente entrada de usuario
+        this.previousValue = this.actualValue || this.previousValue;
+        this.actualValue = '';
     }
 }
 
